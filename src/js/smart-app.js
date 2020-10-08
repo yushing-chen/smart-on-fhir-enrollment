36694dfbf3078edbd4275d0f4e0e6f432c6a0584
L-yushing-chen <69359841+L-yushing-chen@users.noreply.github.com>
2020-10-08 14:08:25 -0400

    Update smart-app.js

--- src/js/smart-app.js
+++ src/js/smart-app.js
@@ -23,7 +23,7 @@
 	      alert(smart.userId);
               alert(smart.tokenResponse.access_token);
               alert(smart.patient);
-                ret.resolve(patient);
+              ret.resolve(patient);
             } else {
               onError();
             }
@@ -32,7 +32,7 @@
         onError();
       }
     }
-    alert("version Rose-10-ES1006-5");
+    alert("versionES-url-1007-2");
     FHIR.oauth2.ready(onReady, onError);
     return ret.promise();
   };
@@ -40,7 +40,7 @@
 //function getPatientEDIPI(patient)
  function getPatientEDIPI(patient) {
       const dsvDodIdentifierSystemName = 'urn:oid:2.16.840.1.113883.3.42.10001.100001.12';
-      let patientId = 'undefined';
+      let patientId = '';
       let found = false;
 
       console.log('extracting the patient identifier EPIPI');
@@ -59,7 +59,7 @@
  function getPatientICN(patient) {
      const dsvIcnIdentifierSystemName = 'urn:oid:2.16.840.1.113883.4.349'
 
-      let patientId = 'undefined';
+      let patientId = '';
       let found = false;
 
       console.log('extracting the patient identifier ICN');
@@ -78,7 +78,7 @@
  function getPatientMRN(patient) {
   const dsvIdentifierSystemName = 'urn:oid:2.16.840.1.113883.3.787.0.0';
   
-      let patientId = 'undefined';
+      let patientId = '';
       let found = false;
 
       console.log('extracting the patient identifier MRN');
@@ -105,7 +105,7 @@
       var deb_id = 'EDIPI=***' + edipi + '***ICN=***' + icn +  '***MRN***='  + mrn + '***' ;
       alert(deb_id);
 //	if ( (edipi == 'undefined' ) && ( icn == 'undefined' ) && ( mrn == 'undefined' )  )
-	if ( (edipi == 'undefined' ) && ( icn == 'undefined' )   )		
+	if ( (edipi == '' ) && ( icn == '' )   )		
 	{   console.log('No patient identifier found');
 		alert('The Patient is missing an EDIPI and ICN. Contact System Administrator.');
       }
@@ -129,8 +129,7 @@
       var dobStr = monthIndex + '/' + day + '/' + year
       console.log(dobStr);
       var fin="";	
-      if (typeof patient.l5 !== 'undefined') 
-         var l5 = patient.l5.toUpperCase();
+
 //Data Url      
 //edipi=12312313&icn=1008834677V755078&name=John+Adams&age=38&dob=10%2F10%2F1961&sex=M&fin=12345544&mrn=33355632221     
 //Hostname Url
@@ -138,7 +137,7 @@
 //https://vaausappesr801.aac.va.gov:7401/es-rs/ratingapp/postform         
 var es_dataUrl = "edipi=" + edipi + "&" + "icn=" + icn + "&" + "name=" + nm + "&" + "dob=" + dobStr + "&" +"sex=" + gender + "&" + "fin=" + fin + "&" + "mrn=" + mrn  ;
 //var es_url = "https://usvadceapp.lcahncrls.net/?data="+ es_dataUrl;
-var es_url = "https://vaww.esrstage1a.aac.va.gov/es-rs/ratingapp/?data="+ es_dataUrl;	  
+var es_url = "https://vaww.esrstage1a.aac.va.gov/es-rs/ratingapp/postform/?data="+ es_dataUrl;	  
 
       console.log(es_url);
       alert(es_url);
